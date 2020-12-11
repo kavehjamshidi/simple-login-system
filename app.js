291b83ec52eb9f93a7518addd05da8303881b8ba
kavehjamshidi <kaveh.jamshidi98@hotmail.com>
2020-12-12 03:26:22 +0330

    added 404 handler

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const userRoutes = require('./routes/userRoutes');
 const sampleProtectedRoute = require('./routes/sampleProtectedRoute');
+const notFoundController = require('./controllers/notFoundController');
 const expressErrorMiddleware = require('./middlewares/errorMiddleware');
 
 // Unhandled promise rejections and uncaught exceptions
@@ -12,6 +13,7 @@ const app = express();
 app.use(express.json());
 app.use('/user', userRoutes);
 app.use('/sample-route', sampleProtectedRoute);
+app.all('*', notFoundController);
 app.use(expressErrorMiddleware);
 
 module.exports = app;
